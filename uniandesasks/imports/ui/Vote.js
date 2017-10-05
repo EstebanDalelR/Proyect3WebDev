734033b190251fe137e6e7e8f9d14a0b420df770
estebandalelr <estebandalelr@gmail.com>
2017-10-05 13:30:16 -0500

    Created Vote Class

---
+++ uniandesasks/imports/ui/Vote.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+/**
+Clase para mostrar los botones de voto y puntaje de una pregunta
+**/
+class Vote extends Component{
+	constructor(props){
+		super(props);
+	}
+
+  castVote(event){
+    if(event==='VoteUp')
+    {
+      // TODO: cast positive
+    }
+    else {
+      //TODO: cast negative
+    }
+  }
+
+	render(){
+		return(
+				<div>
+          <button className='VoteUp btn-group-vertical'>Up</button>
+          <p>{this.props.question.votes}</p>
+          <button className='VoteDown btn-group-vertical'>Down</button>
+        </div>
+			);
+	}
+}
+
+
+export default Vote;
